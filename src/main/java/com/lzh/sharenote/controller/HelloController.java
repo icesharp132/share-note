33bd6e8d16029b9e49423e70562b0ddbcf24b303
luzhaohui <luzhaohui@didichuxing.com>
2017-06-23 21:32:11 +0800

    init

---
+++ src/main/java/com/lzh/sharenote/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.lzh.sharenote.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Lu Zhaohui on 2017/6/23.
+ */
+@RestController
+public class HelloController {
+}
